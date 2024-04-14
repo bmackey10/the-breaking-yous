82347a607a9e015e95d3c1e2643371ec1618bf9e
Brooke <bmackey@nd.edu>
2024-04-14 16:39:10 -0400

    select interests page

--- src/Components/Pages/Register.js
+++ src/Components/Pages/Register.js
@@ -1,4 +1,4 @@
-import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 
 export default function Register() {
     return (
